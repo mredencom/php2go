18c1e811225afe70e4e2ee84e0ee60f7a6e8c101
taoshaoye <mreden@163.com>
2020-07-09 15:58:08 +0800

    add php abs

---
+++ php/abs.go
@@ -0,0 +1,8 @@
+package php
+
+import "math"
+
+// 取出绝对值
+func Abs(x float64) float64 {
+	return math.Abs(x)
+}
---
+++ php/abs_test.go
@@ -0,0 +1,7 @@
+package php
+
+import "testing"
+
+func TestAbs(t *testing.T) {
+	t.Log(Abs(-12.2))
+}
