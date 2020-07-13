224471c159299c887d224cace70b9e5ff0846e5f
taoshaoye <mreden@163.com>
2020-07-13 09:48:57 +0800

    add php empty function

--- php/empty.go
+++ php/empty.go
@@ -21,8 +21,10 @@
 // SOFTWARE.
 package php
 
+// https://www.php.net/manual/zh/function.empty.php
+// empty
 func Empty(s interface{}) bool {
-	if s == "" || s == nil {
+	if s == "" || s == nil || s == 0 {
 		return true
 	}
 	return false
